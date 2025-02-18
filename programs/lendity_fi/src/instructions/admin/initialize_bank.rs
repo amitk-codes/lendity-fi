005ca0bb88882ed843a80213791c3962df5a7c7c
Amit Kumar <amitkcodes@gmail.com>
2025-02-18 13:59:21 +0530

    cargo fmt run

--- programs/lendity_fi/src/instructions/admin/initialize_bank.rs
+++ programs/lendity_fi/src/instructions/admin/initialize_bank.rs
@@ -4,22 +4,22 @@ use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 use crate::{Bank, ANCHOR_DISCRIMINATOR};
 
 #[derive(Accounts)]
-pub struct InitializeBank<'info>{
-  #[account(mut)]
-  pub signer: Signer<'info>,
+pub struct InitializeBank<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
 
-  pub mint: InterfaceAccount<'info, Mint>,
+    pub mint: InterfaceAccount<'info, Mint>,
 
-  #[account(
+    #[account(
     init,
     payer = signer,
     space = ANCHOR_DISCRIMINATOR + Bank::INIT_SPACE,
     seeds = [mint.key().as_ref()],
     bump,
   )]
-  pub bank: Account<'info, Bank>,
+    pub bank: Account<'info, Bank>,
 
-  #[account(
+    #[account(
     init,
     token::mint = mint,
     token::authority = bank_token_account,
@@ -28,17 +28,21 @@ pub struct InitializeBank<'info>{
     seeds = [b"bank_token_account", mint.key().as_ref()],
     bump
   )]
-  pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
 
-  pub system_program: Program<'info, System>,
-  pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
 
-pub fn initialize_bank_handler(ctx: Context<InitializeBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()>{
-  let bank = &mut ctx.accounts.bank;
-  bank.authority = ctx.accounts.signer.key();
-  bank.mint_address = ctx.accounts.mint.key();
-  bank.max_ltv = max_ltv;
-  bank.liquidation_threshold = liquidation_threshold;
-  Ok(())
+pub fn initialize_bank_handler(
+    ctx: Context<InitializeBank>,
+    liquidation_threshold: u64,
+    max_ltv: u64,
+) -> Result<()> {
+    let bank = &mut ctx.accounts.bank;
+    bank.authority = ctx.accounts.signer.key();
+    bank.mint_address = ctx.accounts.mint.key();
+    bank.max_ltv = max_ltv;
+    bank.liquidation_threshold = liquidation_threshold;
+    Ok(())
 }
--- programs/lendity_fi/src/instructions/admin/initialize_user.rs
+++ programs/lendity_fi/src/instructions/admin/initialize_user.rs
@@ -3,26 +3,25 @@ use anchor_lang::prelude::*;
 use crate::{User, ANCHOR_DISCRIMINATOR};
 
 #[derive(Accounts)]
-pub struct InitializeUser<'info>{
-  #[account(mut)]
-  pub signer: Signer<'info>,
+pub struct InitializeUser<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
 
-  #[account(
+    #[account(
     init,
     payer = signer,
     space = ANCHOR_DISCRIMINATOR + User::INIT_SPACE,
     seeds = [signer.key().as_ref()],
     bump,
   )]
-  pub user_account: Account<'info, User>,
+    pub user_account: Account<'info, User>,
 
-  pub system_program: Program<'info, System>,
-
+    pub system_program: Program<'info, System>,
 }
 
-pub fn initialize_user_handler(ctx: Context<InitializeUser>, usdc_address: u64) -> Result<()>{
-  let user_account = &mut ctx.accounts.user_account;
-  user_account.owner = ctx.accounts.signer.key();
-  user_account.usdc_address = usdc_address;
-  Ok(())
+pub fn initialize_user_handler(ctx: Context<InitializeUser>, usdc_address: u64) -> Result<()> {
+    let user_account = &mut ctx.accounts.user_account;
+    user_account.owner = ctx.accounts.signer.key();
+    user_account.usdc_address = usdc_address;
+    Ok(())
 }
